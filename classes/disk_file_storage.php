1e3a8decab0086de57922a780787a78d430a9cbc
Luke Carrier <luke@carrier.im>
2016-08-18 13:58:12 +0100

    Get content files and editor working too

--- classes/disk_file_storage.php
+++ classes/disk_file_storage.php
@@ -11,17 +11,53 @@ namespace local_hvplibraries;
 
 use H5PFileStorage;
 use local_hvplibraries\exception\not_implemented_exception;
+use mod_hvp\file_storage;
 
 defined('MOODLE_INTERNAL') || die;
 
 /**
  * Disk-backed file storage implementation.
  */
-class disk_file_storage implements H5PFileStorage {
+class disk_file_storage extends file_storage implements H5PFileStorage {
+    /**
+     * On-disk path: libraries.
+     *
+     * @var string
+     */
+    const PATH_LIBRARIES = '/local/hvplibraries/libraries';
+
+    /**
+     * Get the contents of a file from within a specified root.
+     *
+     * @param string $root
+     * @param string $file
+     *
+     * @return string
+     */
+    protected static function get_file_contents($root, $file) {
+        global $CFG;
+
+        $path = util::join_paths($CFG->dirroot, $root, $file);
+
+        return file_get_contents($path);
+    }
+
+    /**
+     * Throw on unimplemented method.
+     *
+     * @param string $method
+     *
+     * @throws \local_hvplibraries\exception\not_implemented_exception
+     */
     protected static function throw_unimpl($method) {
         throw new not_implemented_exception($method);
     }
 
+    /**
+     * Warn on implemented method.
+     *
+     * @param string $method
+     */
     protected static function warn_unimpl($method) {
         debugging(util::string('methodnotimplemented', $method));
     }
@@ -30,62 +66,6 @@ class disk_file_storage implements H5PFileStorage {
      * @override \H5PFileStorage
      */
     public function saveLibrary($library) {
-        static::throw_unimpl(__METHOD__);
-    }
-
-    /**
-     * @override \H5PFileStorage
-     */
-    public function saveContent($source, $id) {
-        static::throw_unimpl(__METHOD__);
-    }
-
-    /**
-     * @override \H5PFileStorage
-     */
-    public function deleteContent($id) {
-        static::throw_unimpl(__METHOD__);
-    }
-
-    /**
-     * @override \H5PFileStorage
-     */
-    public function cloneContent($id, $newId) {
-        static::throw_unimpl(__METHOD__);
-    }
-
-    /**
-     * @override \H5PFileStorage
-     */
-    public function getTmpPath() {
-        static::throw_unimpl(__METHOD__);
-    }
-
-    /**
-     * @override \H5PFileStorage
-     */
-    public function exportContent($id, $target) {
-        static::throw_unimpl(__METHOD__);
-    }
-
-    /**
-     * @override \H5PFileStorage
-     */
-    public function exportLibrary($library, $target) {
-        static::throw_unimpl(__METHOD__);
-    }
-
-    /**
-     * @override \H5PFileStorage
-     */
-    public function saveExport($source, $filename) {
-        static::throw_unimpl(__METHOD__);
-    }
-
-    /**
-     * @override \H5PFileStorage
-     */
-    public function deleteExport($filename) {
         static::throw_unimpl(__METHOD__);
     }
 
@@ -113,35 +93,14 @@ class disk_file_storage implements H5PFileStorage {
     /**
      * @override \H5PFileStorage
      */
-    public function getContent($file_path) {
-        static::throw_unimpl(__METHOD__);
-    }
+    public function getContent($filepath) {
+        /* This is ugly, but necessary for the editor to work. It would be nice
+         * if we had domain-specific \H5PFileStorage implementations for each
+         * file areas. */
+        if ($resolvedfile = util::starts_with($filepath, '/libraries')) {
+            return static::get_file_contents(static::PATH_LIBRARIES, $resolvedfile);
+        }
 
-    /**
-     * @override \H5PFileStorage
-     */
-    public function saveFile($file, $contentId) {
-        static::throw_unimpl(__METHOD__);
-    }
-
-    /**
-     * @override \H5PFileStorage
-     */
-    public function cloneContentFile($file, $fromId, $toId) {
-        static::throw_unimpl(__METHOD__);
-    }
-
-    /**
-     * @override \H5PFileStorage
-     */
-    public function getContentFile($file, $contentId) {
-        static::throw_unimpl(__METHOD__);
-    }
-
-    /**
-     * @override \H5PFileStorage
-     */
-    public function removeContentFile($file, $contentId) {
-        static::throw_unimpl(__METHOD__);
+        parent::getContent($filepath);
     }
 }
--- classes/util.php
+++ classes/util.php
@@ -25,6 +25,20 @@ class util {
     const MOODLE_COMPONENT = 'local_hvplibraries';
 
     /**
+     * Join path fragments with the platform's directory separator.
+     *
+     * @param string[] ...$paths
+     *
+     * @return string
+     */
+    public static function join_paths(...$paths) {
+        /* We're using '/' instead of DIRECTORY_SEPARATOR because H5P tends to
+         * derive lots of paths from URLs. We'll let PHP normalise paths for us
+         * for the time being. */
+        return implode('/', $paths);
+    }
+
+    /**
      * Get a language string.
      *
      * @param string           $string The name of the string to retrieve.
@@ -59,5 +73,22 @@ class util {
     public static function real_string($string, $a=null, $module=null) {
         $module = $module ?: static::MOODLE_COMPONENT;
         return get_string($string, $module, $a);
+    }
+
+    /**
+     * Does the string start with the substring?
+     *
+     * @param string $string    The larger of the two strings.
+     * @param string $substring The substring to match at the beginning of the
+     *                          string.
+     *
+     * @return boolean|string The remaining text after the substring if matched,
+     *                        else false.
+     */
+    public static function starts_with($string, $substring) {
+        if (substr_compare($string, $substring, 0) > 0) {
+            return substr($string, strlen($substring));
+        }
+        return false;
     }
 }
